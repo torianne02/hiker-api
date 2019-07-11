3db990e5e7a099f30971d69cc1e1434d15af9a54
torianne02 <victoria.fluharty@gmail.com>
2019-07-11 14:52:54 -0700

    adds logic to factories/hikes.rb adds var info for tests via FactoryBot

---
+++ spec/factories/hikes.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do 
+    factory :hike do 
+        name { Faker::Movies::StarWars.character }
+        location { Faker::Movies::StarWars.planet }
+        distance { Faker::Number.decimal(1) }
+        elevation_gain { Faker::Number.number(4) }
+        date_completed { Faker::Date.backward(14) }
+        user_id 1
+    end 
+end 
