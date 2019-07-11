29fd914f98590ae924b56d931913b1b1d93cce22
torianne02 <victoria.fluharty@gmail.com>
2019-07-10 18:09:11 -0700

    adds dependency to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password
-    has_many :hikes
+    has_many :hikes, dependent: :destroy
 
     validates :name, :username, :age, :gender, :password, :presence => true 
 
