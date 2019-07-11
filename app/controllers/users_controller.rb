8eacc5a4e2b5ce5bbbeb1d71b52ab553a559ea5c
torianne02 <victoria.fluharty@gmail.com>
2019-07-11 15:37:57 -0700

    adds private methods to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+    before_action set_user, only: [:show, :update, :destroy]
+
+    private 
+
+    def user_params
+        params.permit(:name, :username, :password, :age, :gender)
+    end 
+    
+    def set_user
+        @user = User.find(params[:id])
+    end 
 end
