eec5eb2858d886be68d46e2514f9fe34ffd8b710
torianne02 <victoria.fluharty@gmail.com>
2019-07-10 17:59:05 -0700

    fixes validation tests for hike model

--- spec/models/hike_spec.rb
+++ spec/models/hike_spec.rb
@@ -6,5 +6,9 @@ RSpec.describe Hike, type: :model do
     it { should belong_to(:user) }
 
     # Validation test
-    it { should validate_presence_of(:name, :location, :distance, :elevation_gain, :date_completed) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:location) }
+    it { should validate_presence_of(:distance) }
+    it { should validate_presence_of(:elevation_gain) }
+    it { should validate_presence_of(:date_completed) }
 end 
