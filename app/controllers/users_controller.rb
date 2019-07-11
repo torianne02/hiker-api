3ac033be5c315c76a57c79b783f87337e77d7423
torianne02 <victoria.fluharty@gmail.com>
2019-07-11 15:45:05 -0700

    adds logic to users_controller adds logic for HTTP requests: GET, POST, PUT, DELETE

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,33 @@
 class UsersController < ApplicationController
     before_action set_user, only: [:show, :update, :destroy]
 
+    # GET /users
+    def index
+        @users = User.all
+        json_response(@users)
+    end 
+
+    # POST /users
+    def create
+        @user = User.create!(user_params)
+        json_response(@user)
+    end 
+
+    # GET /users/:id
+    def show
+        json_response(@user)
+    end 
+
+    # PUT /users/:id
+    def update
+        @user.update(user_params)
+    end 
+
+    # DELETE /users/:id 
+    def destroy
+        @user.destroy
+    end
+
     private 
 
     def user_params
