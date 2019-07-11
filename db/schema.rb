f82fb4a982c9e01487460ceb7ac3a626120ca0a1
torianne02 <victoria.fluharty@gmail.com>
2019-07-10 18:06:14 -0700

    updates user table to accept password_digest removes password column adds password_digest column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_005953) do
+ActiveRecord::Schema.define(version: 2019_07_11_010434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,9 +27,9 @@ ActiveRecord::Schema.define(version: 2019_07_10_005953) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
-    t.string "password"
     t.integer "age"
     t.string "gender"
+    t.string "password_digest"
   end
 
 end
