e7b75cf0cef30aeffcf6f4af2929ead2a013ad50
torianne02 <victoria.fluharty@gmail.com>
2019-07-15 13:56:36 -0700

    adds test logic for users/user_id/hikes

--- spec/requests/hikes_spec.rb
+++ spec/requests/hikes_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Hikes API' do 
+  # initialize test data
+  let!(:user) { create(:user) }
+  let!(:hikes) { create_list(:hike, 20, user_id: user.id) }
+  let(:user_id) { user.id }
+  let(:id) { hikes.first.id }
 
+  # test suite for GET /users/:user_id/hikes
+  describe 'GET /users/:user_id/hikes' do
+    before { get "/users/#{user_id}/hikes" }
+    
+    context 'when user exists' do
+      it 'returns status code 200' do 
+        expect(response).to have_http_status(200)
+      end 
+
+      it 'returns all user hikes' do
+        expect(json.size).to eq(20)
+      end 
+    end
 end 
