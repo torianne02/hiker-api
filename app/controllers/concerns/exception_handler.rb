2ebd3ca9a7960750d3c12d1f658f779d4a07b7d6
torianne02 <victoria.fluharty@gmail.com>
2019-07-16 14:03:42 -0700

    defines custom error subclasses in exception handler file

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,14 +1,18 @@
 module ExceptionHandler
-    # provides included method
-    extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
 
-    included do 
-        rescue_from ActiveRecord::RecordNotFound do |e|
-            json_response({ message: e.message }, :not_found)
-        end
+  # define custom error subclasses
+  class AuthenticationError < StandardError; end 
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end 
 
-        rescue_from ActiveRecord::RecordInvalid do |e|
-            json_response({ message: e.message }, :unprocessable_entity)
-        end 
+  included do 
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      json_response({ message: e.message }, :not_found)
+      end
+
+    rescue_from ActiveRecord::RecordInvalid do |e|
+        json_response({ message: e.message }, :unprocessable_entity)
     end 
+  end 
 end 
