de2871171a07f26b397f6742408b688e82f44ca1
torianne02 <victoria.fluharty@gmail.com>
2019-07-18 16:17:39 -0700

    uncomments code for signup authen in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::API
   include Response
   include ExceptionHandler
 
-  # before_action :authorize_request
-  # attr_reader :current_user
+  before_action :authorize_request
+  attr_reader :current_user
 
-  # private
+  private
 
-  # # check for valid token and return user
-  # def authorize_request
-  #   @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
-  # end 
+  # check for valid token and return user
+  def authorize_request
+    @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  end 
 end
