5c0482dc5781af5e7f0a9e2b745bf025c5617637
torianne02 <victoria.fluharty@gmail.com>
2019-07-17 17:56:58 -0700

    adds test logic to authentication_spec for authentication controller

---
+++ spec/requests/authentication_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe 'Authentication', type: :request do
+  # authentication test suite
+  describe 'POST /auth/login' do
+    # create test user
+    let!(:user) { create(:user) }
+
+    # set headers for authorization
+    let(:headers) { valid_headers.except('Authorization') }
+
+    # set test valid credentials
+    let(:valid_credentials) do
+      {
+        username: user.username,
+        password: user.password
+      }.to_json
+    end 
+
+    # set test invalid credentials
+    let(:invalid_credentials) do 
+      {
+        username: Faker::Internet.username,
+        password: Faker::Internet.password
+      }.to_json 
+    end 
+
+    # set request.headers to our custom headers
+    # before { allow(request).to receive(:headers).and_return(headers) }
+
+    # returns auth token when request is valid
+    context 'When request is valid' do
+      before { post '/auth/login', params: valid_credentials, headers: headers }
+
+      it 'returns an authentication token' do 
+        expect(json['auth_token']).not_to be_nil
+      end 
+    end 
+
+    # returns failure message when request is invalid
+    context 'When request is invalid' do 
+      before { post '/auth/login', params: invalid_credentials, headers: headers }
+
+      it 'returns a failure message' do
+        expect(json['message']).to match(/Invalid credentials/)
+      end 
+    end 
+  end 
+end 
