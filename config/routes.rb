730d94ae2853791ff636a35034d5098a68d0e3bd
torianne02 <victoria.fluharty@gmail.com>
2019-07-22 14:22:41 -0700

    creates api namespace for controllers in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :users do
-    resources :hikes
+  
+  # api namespace for controllers
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :users do
+      resources :hikes
+    end
   end
 
   post 'auth/login', to: 'authentication#authenticate'
