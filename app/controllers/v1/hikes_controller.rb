ec7f28e29ca2a2b31c640c4d66bc6e3a1b1e00da
torianne02 <victoria.fluharty@gmail.com>
2019-07-22 14:57:54 -0700

    updates hikes and users controllers to be V1:: instead of module V1 current error is NameError (uninitialized constant ApiVersion) ... GRRR

--- app/controllers/v1/hikes_controller.rb
+++ app/controllers/v1/hikes_controller.rb
@@ -1,48 +1,46 @@
-module V1
-  class HikesController < ApplicationController
-    before_action :set_user
-    before_action :set_hike, only: [:show, :update, :destroy]
+class V1::HikesController < ApplicationController
+  before_action :set_user
+  before_action :set_hike, only: [:show, :update, :destroy]
 
-    # GET /users/:user_id/hikes 
-    def index 
-      json_response(@user.hikes)
-    end 
+  # GET /users/:user_id/hikes 
+  def index 
+    json_response(@user.hikes)
+  end 
 
-    # GET /users/:user_id/hikes/:id
-    def show 
-      json_response(@hike)
-    end 
+  # GET /users/:user_id/hikes/:id
+  def show 
+    json_response(@hike)
+  end 
 
-    # POST /users/:user_id/hikes
-    def create
-      @user.hikes.create!(hike_params)
-      json_response(@user, :created)
-    end 
+  # POST /users/:user_id/hikes
+  def create
+    @user.hikes.create!(hike_params)
+    json_response(@user, :created)
+  end 
 
-    # PUT /users/:user_id/hikes/:id
-    def update
-      @hike.update(hike_params)
-      head :no_content
-    end 
+  # PUT /users/:user_id/hikes/:id
+  def update
+    @hike.update(hike_params)
+    head :no_content
+  end 
 
-    # DELETE /users/:user_id/hikes/:id 
-    def destroy
-      @hike.destroy
-      head :no_content 
-    end 
+  # DELETE /users/:user_id/hikes/:id 
+  def destroy
+    @hike.destroy
+    head :no_content 
+  end 
 
-    private
+  private
 
-    def hike_params
-      params.permit(:name, :location, :distance, :date_completed, :elevation_gain)
-    end 
+  def hike_params
+    params.permit(:name, :location, :distance, :date_completed, :elevation_gain)
+  end 
 
-    def set_user 
-      @user = User.find(params[:user_id])
-    end 
+  def set_user 
+    @user = User.find(params[:user_id])
+  end 
 
-    def set_hike 
-      @hike = @user.hikes.find_by!(id: params[:id]) if @user
-    end 
-  end
+  def set_hike 
+    @hike = @user.hikes.find_by!(id: params[:id]) if @user
+  end 
 end
--- app/controllers/v2/users_controller.rb
+++ app/controllers/v2/users_controller.rb
@@ -1,2 +1,5 @@
 class V2::UsersController < ApplicationController
+  def index
+    json_response({ message: "Hola" })
+  end
 end
