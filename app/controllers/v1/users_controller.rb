b32786b5d84eaecdcbc5eb8b1f6cc7fb189f3c82
torianne02 <victoria.fluharty@gmail.com>
2019-07-22 14:26:27 -0700

    adds module V1 to hikes and users controller

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,49 +1,51 @@
-class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :update, :destroy]
-  skip_before_action :authorize_request, only: :create
+module V1  
+  class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :update, :destroy]
+    skip_before_action :authorize_request, only: :create
 
-  # WILL implement show, update, and destroy functionality in the future - - maybe index
+    # WILL implement show, update, and destroy functionality in the future - - maybe index
 
-  # GET /users
-  def index
-    @users = User.all
-    json_response(@users)
-  end 
+    # GET /users
+    def index
+      @users = User.all
+      json_response(@users)
+    end 
 
-  # POST /users
-  def create
-    @user = User.create!(user_params)
-    
-    auth_token = AuthenticateUser.new(@user.username, @user.password)
-    
-    response = { message: Message.account_created, auth_token: auth_token}
-    
-    json_response(response, :created)
-    # json_response(@user)
-  end 
+    # POST /users
+    def create
+      @user = User.create!(user_params)
+      
+      auth_token = AuthenticateUser.new(@user.username, @user.password)
+      
+      response = { message: Message.account_created, auth_token: auth_token}
+      
+      json_response(response, :created)
+      # json_response(@user)
+    end 
 
-  # GET /users/:id
-  def show
-    json_response(@user)
-  end 
+    # GET /users/:id
+    def show
+      json_response(@user)
+    end 
 
-  # PUT /users/:id
-  def update
-    @user.update(user_params)
-  end 
+    # PUT /users/:id
+    def update
+      @user.update(user_params)
+    end 
 
-  # DELETE /users/:id 
-  def destroy
-    @user.destroy
+    # DELETE /users/:id 
+    def destroy
+      @user.destroy
+    end
+
+    private 
+
+    def user_params
+      params.permit(:name, :username, :password, :age, :gender)
+    end 
+
+    def set_user
+      @user = User.find(params[:id])
+    end 
   end
-
-  private 
-
-  def user_params
-    params.permit(:name, :username, :password, :age, :gender)
-  end 
-
-  def set_user
-    @user = User.find(params[:id])
-  end 
 end
