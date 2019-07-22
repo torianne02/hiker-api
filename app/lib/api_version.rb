51285b9c005a7007cbfb7787798aa9fd30bb47f4
torianne02 <victoria.fluharty@gmail.com>
2019-07-22 14:19:56 -0700

    adds logic to check version and headers in api_version

--- app/lib/api_version.rb
+++ app/lib/api_version.rb
@@ -5,4 +5,17 @@ class ApiVersion
     @version = version
     @default = default 
   end 
+
+  # check if version is specified or is default
+  def matches?(request)
+    check_headers(request.headers) || default
+  end 
+
+  private 
+
+  # check v from Accept headers; expect custom media type 'users'
+  def check_headers(headers)
+    accept = headers[:accept]
+    accept && accept.include?("application/vnd.users.#{version}+json")
+  end 
 end 
