bbd9371fbb24c72743aae88bf73782b9eda34cd8
torianne02 <victoria.fluharty@gmail.com>
2019-07-22 14:40:57 -0700

    creates v2/users controller and specs adds route

---
+++ app/controllers/v2/users_controller.rb
@@ -0,0 +1,2 @@
+class V2::UsersController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  
   # api namespace for controllers
   scope module: :v1, constraints: ApiVersion.new('v1', true) do
     resources :users do
       resources :hikes
     end
   end
+
+  scope module: :v2, constraints: ApiVersion.new('v2') do 
+    resources :users, only: :index
+  end 
 
   post 'auth/login', to: 'authentication#authenticate'
 
