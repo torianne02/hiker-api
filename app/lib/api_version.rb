afe54b3bdcd8d59037c54a06edd2fdd6c864eb69
torianne02 <victoria.fluharty@gmail.com>
2019-07-22 14:41:11 -0700

    fixes spacing/comments

--- app/lib/api_version.rb
+++ app/lib/api_version.rb
@@ -3,19 +3,18 @@ class ApiVersion
 
   def initialize(version, default = false)
     @version = version
-    @default = default 
-  end 
+    @default = default
+  end
 
-  # check if version is specified or is default
+  # check whether version is specified or is default
   def matches?(request)
     check_headers(request.headers) || default
-  end 
+  end
 
-  private 
+  private
 
-  # check v from Accept headers; expect custom media type 'users'
   def check_headers(headers)
     accept = headers[:accept]
     accept && accept.include?("application/vnd.users.#{version}+json")
-  end 
-end 
+  end
+end
