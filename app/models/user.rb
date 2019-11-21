da71842bfc72fa314ac687756410ae74e8bcbd5d
torianne02 <victoria.fluharty@gmail.com>
2019-11-20 16:49:43 -0800

    updates AR associations and validations in user  model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ require 'date'
 
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :hikes, dependent: :destroy
+  has_many :user_hikes
+  has_many :hikes, through: :user_hikes
 
-  validates :name, :username, :birthday, :gender, :password, :presence => true 
-  validates :username, :uniqueness => true
+  validates :name, :username, :birthday, :gender, :password, :email, :presence => true 
+  validates :username, :email, :uniqueness => true
 
   def age 
     ((Time.now - birthday.to_time)/(60*60*24*365)).floor
