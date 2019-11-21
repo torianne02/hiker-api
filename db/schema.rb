908ed994176bcfee5457430692e5791e62f3c4e0
torianne02 <victoria.fluharty@gmail.com>
2019-11-20 17:07:01 -0800

    updates schema to include changes to parks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_002823) do
+ActiveRecord::Schema.define(version: 2019_11_21_004703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,8 @@ ActiveRecord::Schema.define(version: 2019_11_21_002823) do
 
   create_table "parks", force: :cascade do |t|
     t.string "name"
-    t.string "type"
     t.string "location"
+    t.string "park_type"
   end
 
   create_table "user_hikes", force: :cascade do |t|
