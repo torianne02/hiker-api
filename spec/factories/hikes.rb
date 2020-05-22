49c368c88b9fac395150c2c0f4049e12126630d9
torianne02 <victoria.fluharty@gmail.com>
2020-05-22 11:07:30 -0700

    updates hike model tests and factory removes old attribute validations updates hike association tests updates factory file by removing unnecessary attributes

--- spec/factories/hikes.rb
+++ spec/factories/hikes.rb
@@ -1,10 +1,7 @@
 FactoryBot.define do 
   factory :hike do 
     name { Faker::Movies::StarWars.character }
-    location { Faker::Movies::StarWars.planet }
-    distance { Faker::Number.decimal(1) }
-    elevation_gain { Faker::Number.number(4) }
-    date_completed { Faker::Date.backward(14) }
+    # location { Faker::Movies::StarWars.planet }
     # user_id nil
   end 
 end 
--- spec/models/hike_spec.rb
+++ spec/models/hike_spec.rb
@@ -3,12 +3,8 @@ require 'rails_helper'
 # Test suite for the Hike model
 RSpec.describe Hike, type: :model do
   # association test
-  it { should belong_to(:user) }
+  it { should have_many(:user_hikes) }
 
   # Validation test
   it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:location) }
-  it { should validate_presence_of(:distance) }
-  it { should validate_presence_of(:elevation_gain) }
-  it { should validate_presence_of(:date_completed) }
 end 
