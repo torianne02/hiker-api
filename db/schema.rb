409cc2ac9b250165c3b1b991686beb5b9a2ce0f8
torianne02 <victoria.fluharty@gmail.com>
2020-05-28 15:20:10 -0700

    adds park_id to hikes table updates schema adds association test in models/hike_spec.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_220725) do
+ActiveRecord::Schema.define(version: 2020_05_28_221757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_05_28_220725) do
   create_table "hikes", force: :cascade do |t|
     t.string "name"
     t.string "location"
+    t.integer "park_id"
   end
 
   create_table "parks", force: :cascade do |t|
--- spec/models/hike_spec.rb
+++ spec/models/hike_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 RSpec.describe Hike, type: :model do
   # association test
   it { should have_many(:user_hikes) }
+  it { should belong_to(:park) }
 
   # Validation test
   it { should validate_presence_of(:name) }
