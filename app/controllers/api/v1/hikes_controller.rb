cc93e71d6f7e16763e25db4f14650a0154d1fff4
torianne02 <victoria.fluharty@gmail.com>
2020-05-29 17:25:56 -0700

    updates hikes controller and related tests updates controller to accept new routes removes user relation code

--- app/controllers/api/v1/hikes_controller.rb
+++ app/controllers/api/v1/hikes_controller.rb
@@ -1,30 +1,31 @@
 class Api::V1::HikesController < ApplicationController
-  before_action :set_user
+  before_action :set_park
   before_action :set_hike, only: [:show, :update, :destroy]
 
-  # GET /users/:user_id/hikes 
+  # GET /parks/:park_id/hikes 
   def index 
-    json_response(@user.hikes)
+    @hikes = Hike.all
+    json_response(@park.hikes)
   end 
 
-  # GET /users/:user_id/hikes/:id
+  # GET /parks/:park_id/hikes/:id
   def show 
     json_response(@hike)
   end 
 
-  # POST /users/:user_id/hikes
+  # POST /parks/:park_id/hikes
   def create
-    @user.hikes.create!(hike_params)
-    json_response(@user, :created)
+    @park.hikes.create!(hike_params)
+    json_response(@park, :created)
   end 
 
-  # PUT /users/:user_id/hikes/:id
+  # PUT /parks/:park_id/hikes/:id
   def update
     @hike.update(hike_params)
     head :no_content
   end 
 
-  # DELETE /users/:user_id/hikes/:id 
+  # DELETE /parks/:park_id/hikes/:id 
   def destroy
     @hike.destroy
     head :no_content 
@@ -33,14 +34,14 @@ class Api::V1::HikesController < ApplicationController
   private
 
   def hike_params
-    params.permit(:name, :location, :distance, :date_completed, :elevation_gain)
-  end 
-
-  def set_user 
-    @user = User.find(params[:user_id])
+    params.permit(:name, :park_id)
   end 
 
   def set_hike 
-    @hike = @user.hikes.find_by!(id: params[:id]) if @user
+    @hike = @park.hikes.find_by!(id: params[:id]) if @park
   end 
+
+  def set_park
+    @park = Park.find(params[:park_id])
+  end
 end
